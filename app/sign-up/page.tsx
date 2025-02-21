3ee52695a21fc571c7ef10c831bce114def20b9f
c2e-charlie <charlie@c2eprotocol.com>
2025-02-21 19:19:34 +0900

    fix: 작업 중단 TODO 메모

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MobileWrapper from "../layout/MobileWrapper";
+import { Typography } from "@mui/material";
 
 function SignUp() {
   return (
     <MobileWrapper>
-      <div>test message</div>
+      {/* TODO: 작업 여기부터 시작 (02/21) */}
+      <Typography variant="h6">회원 가입 유형을 선택해주세요</Typography>
     </MobileWrapper>
   );
 }
