7969c672a05eb3eee177454f88671a62a16389f7
sonewdim <sonewdim@sonewdimui-MacBookPro.local>
2025-03-29 18:32:40 +0900

    feat: 비밀번호 초기화 기능 구현

--- app/password-reset/page.tsx
+++ app/password-reset/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import MobileWrapper from "../layout/MobileWrapper";
 import {
   Alert,
@@ -11,9 +11,93 @@ import {
   TextField,
   Typography,
   Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
 } from "@mui/material";
+import { useVerifyEmailMutation } from "../hooks/apis/useVerifyEmail";
+import { useResetPasswordMutation } from "../hooks/apis/useResetPassword";
+import { useRouter } from "next/navigation";
 
 function PasswordReset() {
+  const router = useRouter();
+  const {mutate: verifyEmailMutate} = useVerifyEmailMutation();
+  const {mutate: resetPasswordMutate} = useResetPasswordMutation();
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    if (emailError !== '') {
+      setEmailError('');
+    }
+
+    setEmail(e.target.value);
+  }
+
+  const [isVerifyCodeSent, setIsVerifyCodeSent] = useState(false);
+
+  const handleVerifyEmail = () => {
+    if(email === '') {
+      setEmailError("이메일을 입력해주세요.");
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)) {
+      setEmailError("올바른 이메일 형식을 입력해주세요.");
+      return;
+    }
+
+    verifyEmailMutate({
+      body: { email }
+    }, {
+      onSuccess: () => {
+        setIsVerifyCodeSent(true);
+      },
+      onError: (error: any) => {
+        switch(error.details.errorCode) {
+          case 'NO_EXISTS_EMAIL_USER':
+            setEmailError("해당 이메일의 계정이 존재하지 않습니다");
+            return;
+          default:
+            alert(`이메일 인증번호 전송이 실패하였습니다.: ${JSON.stringify(error)}`);
+        }
+      }
+    })
+  }
+
+  const [verifyCode, setVerifyCode] = useState('');
+  const [verifyCodeError, setVerifyCodeError] = useState('');
+  const [verifySuccess, setVerifySuccess] = useState(true);
+  const handleChangeVerifyCode = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.value.length > 6) {
+      return;
+    }
+
+    setVerifyCode(e.target.value);
+  }
+
+  const handleResetPassword = () => {
+    resetPasswordMutate({
+      body: {
+        email,
+        verifyCode
+      }
+    }, {
+      onSuccess: () => {
+        setVerifySuccess(true);
+      },
+      onError: () => {
+        setVerifyCodeError('인증번호를 다시 확인해주세요')
+      }
+    })
+  }
+
+  const handleVerifySuccessDialogClose = () => {
+    router.replace('/sign-in');
+  }
+
   return (
     <MobileWrapper>
       <Alert variant="standard" severity="info">
@@ -42,11 +126,22 @@ function PasswordReset() {
         <Box height={8} />
         <Divider />
         <Box height={32} />
-        <TextField fullWidth variant="standard" label="이메일" />
+        <TextField fullWidth variant="standard" label="이메일" onChange={handleChangeEmail} disabled={isVerifyCodeSent} error={emailError !== ''} helperText={emailError} />
         <Box height={16} />
-        <Button fullWidth variant="contained" size="large" color="primary">
+        <Button fullWidth variant="contained" size="large" color="primary" onClick={handleVerifyEmail} disabled={isVerifyCodeSent || emailError !== ''}>
           인증번호 발송
         </Button>
+        <Box height={12} />
+        {isVerifyCodeSent && <Button fullWidth variant="outlined" size="large" color="primary" onClick={() => setIsVerifyCodeSent(false)}>
+          인증번호 재발송
+        </Button>}
+        <Box height={12} />
+        {isVerifyCodeSent && <Alert variant="standard">
+          <AlertTitle>인증번호 발송 성공</AlertTitle>
+          <Typography variant="body2">
+            메일함에서 인증번호를 확인하여 아래에 입력해주세요.
+          </Typography>
+        </Alert>}
 
         <Box height={56} />
 
@@ -54,12 +149,20 @@ function PasswordReset() {
         <Box height={8} />
         <Divider />
         <Box height={32} />
-        <TextField fullWidth variant="standard" label="인증번호 확인" />
+        <TextField fullWidth variant="standard" label="인증번호 확인" onChange={handleChangeVerifyCode} error={verifyCodeError !== ''} helperText={verifyCodeError}/>
         <Box height={16} />
-        <Button fullWidth variant="contained" size="large" color="primary">
+        <Button fullWidth variant="contained" size="large" color="primary" onClick={handleResetPassword} disabled={email === '' || !isVerifyCodeSent || verifyCode === ''}>
           새로운 비밀번호 발송
         </Button>
       </Paper>
+
+      <Dialog open={verifySuccess} fullWidth>
+      <DialogTitle>새로운 비밀번호 발송 완료</DialogTitle>
+      <DialogContent>새로운 비밀번호가 발송되었습니다.<br/>메일함에서 비밀번호를 확인하여 로그인해주세요.</DialogContent>
+      <DialogActions>
+        <Button onClick={handleVerifySuccessDialogClose}>확인</Button>
+      </DialogActions>
+    </Dialog>
     </MobileWrapper>
   );
 }
