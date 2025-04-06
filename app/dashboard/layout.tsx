563697b4a36653aab740799744f1bc2f949a0961
c2e-charlie <45479579+bluetg2001@users.noreply.github.com>
2025-04-07 02:16:13 +0900

    fix: 교수 대쉬보드 목록 수정

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -41,6 +41,7 @@ const routeMap = {
     { label: "비밀번호 변경", path: "/dashboard/password-change" },
   ],
   PROFESSOR: [
+    { label: "회원 정보", path: "/dashboard/member-info" },
     { label: "비밀번호 변경", path: "/dashboard/password-change" },
     { label: "통계 요약", path: "/dashboard/analytics" },
   ],
