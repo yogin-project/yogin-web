6bc54bb2b8f94e1c55d8d48184e0483d34d8b7da
c2e-charlie <45479579+bluetg2001@users.noreply.github.com>
2025-04-13 22:49:25 +0900

    feat: api 경로 변경

--- app/api/auth/reset-password/route.ts
+++ app/api/auth/reset-password/route.ts
@@ -1,29 +1,29 @@
 import { NextResponse } from "next/server";
 
 export async function PATCH(req: Request) {
-    try {
-        const body = await req.json();
-        const API_URL = process.env.API_URL;
+  try {
+    const body = await req.json();
+    const API_URL = process.env.API_URL;
 
-        const response = await fetch(`${API_URL}/password/reset`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body)
-        });
+    const response = await fetch(`${API_URL}/auth/password/reset`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if(!response.ok) {
-            return NextResponse.json(data, {status: response.status})
-        }
-
-        return NextResponse.json(data, {status: 200});
-    } catch (error) {
-        return NextResponse.json(
-            {message: "Internal Server Error", error},
-            {status: 500}
-        )
+    if (!response.ok) {
+      return NextResponse.json(data, { status: response.status });
     }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
--- app/hooks/apis/useResetPassword.ts
+++ app/hooks/apis/useResetPassword.ts
@@ -1,17 +1,17 @@
-import { useApiManager } from "@/app/libs/apiManager"
+import { useApiManager } from "@/app/libs/apiManager";
 
 export const useResetPasswordMutation = () => {
-    return useApiManager({
-        method: "PATCH",
-        path: "auth/reset-password",
+  return useApiManager({
+    method: "PATCH",
+    path: "auth/password/reset",
 
-        mutationOption: {
-            onSuccess: (data: any) => {
-                console.log("비밀번호 초기화 완료:", data);
-            },
-            onError: (error: any) => {
-                console.log("비밀번호 초기화 실패:", error);
-            }
-        }
-    }).setUseMutation;
-}
+    mutationOption: {
+      onSuccess: (data: any) => {
+        console.log("비밀번호 초기화 완료:", data);
+      },
+      onError: (error: any) => {
+        console.log("비밀번호 초기화 실패:", error);
+      },
+    },
+  }).setUseMutation;
+};
