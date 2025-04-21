92fe1042e8bf49651ca936140b01c9d8b37cddf7
kyngmn <kmoffi@naver.com>
2025-04-22 04:51:43 +0900

    fix: 41pg / 교수 회원 가입 폼 UI 수정

--- app/sign-up/professor/page.tsx
+++ app/sign-up/professor/page.tsx
@@ -3,8 +3,9 @@
 import {
   Box,
   Button,
-  Divider,
+  FormControl,
   IconButton,
+  InputLabel,
   MenuItem,
   Paper,
   Select,
@@ -16,7 +17,6 @@ import React, { useState } from "react";
 
 import AddressSearch from "@/app/components/AddSearch";
 import AgreementSection from "../_components/AgreementSection";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Image from "next/image";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
@@ -158,8 +158,7 @@ function SignUpProfessor() {
     formData.organization &&
     formData.address &&
     formData.position &&
-    formData.file &&
-    isEmailChecked;
+    formData.file;
 
   return (
     <MobileWrapper>
@@ -172,19 +171,16 @@ function SignUpProfessor() {
           mb: 4,
         }}
       >
-        <Typography variant="h6" mb={1}>
+        <Typography variant="h6" mb={3}>
           전문가 회원가입
         </Typography>
 
         <Box
           component="form"
           onSubmit={handleSignUp}
-          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          sx={{ display: "flex", flexDirection: "column", gap: 4 }}
         >
-          {/* ✅ 계정정보 */}
-          <Typography variant="body1">계정정보</Typography>
-          <Divider />
-          <Stack flexDirection={"row"} gap={2} mt={2}>
+          <Stack gap={1.5}>
             <TextField
               name="email"
               variant="standard"
@@ -193,126 +189,119 @@ function SignUpProfessor() {
               sx={{ flex: 1 }}
               onChange={handleInputChange}
             />
-            <Button
-              variant="contained"
-              size="small"
-              onClick={() => handleCheckEmail(formData.email)}
-            >
-              중복 확인
-            </Button>
+            <TextField
+              name="password"
+              variant="standard"
+              label="비밀번호"
+              type="password"
+              fullWidth
+              onChange={handleInputChange}
+              error={passwordError}
+              helperText={
+                passwordError
+                  ? "비밀번호가 일치하지 않습니다."
+                  : "비밀번호는 영문자 포함 8자리 이상 입력해주세요."
+              }
+            />
+            <TextField
+              name="confirmPassword"
+              variant="standard"
+              label="비밀번호 확인"
+              type="password"
+              fullWidth
+              onChange={handleInputChange}
+              error={passwordError}
+              helperText={
+                passwordError
+                  ? "비밀번호가 일치하지 않습니다."
+                  : "비밀번호를 한 번 더 입력해주세요."
+              }
+            />
           </Stack>
 
-          <TextField
-            name="password"
-            variant="standard"
-            label="비밀번호"
-            type="password"
-            fullWidth
-            onChange={handleInputChange}
-            error={passwordError}
-            helperText={
-              passwordError
-                ? "비밀번호가 일치하지 않습니다."
-                : "비밀번호는 영문자 포함 8자리 이상 입력해주세요."
-            }
-          />
-          <TextField
-            name="confirmPassword"
-            variant="standard"
-            label="비밀번호 확인"
-            type="password"
-            fullWidth
-            onChange={handleInputChange}
-            error={passwordError}
-            helperText={
-              passwordError
-                ? "비밀번호가 일치하지 않습니다."
-                : "비밀번호를 한 번 더 입력해주세요."
-            }
-          />
-          <TextField
-            name="name"
-            variant="standard"
-            label="이름"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <TextField
-            name="phoneNumber"
-            variant="standard"
-            label="휴대폰 번호"
-            fullWidth
-            onChange={handleInputChange}
-          />
-
-          {/* ✅ 소속 정보 */}
-          <Typography variant="body1" mt={4}>
-            학교 / 소속 정보
-          </Typography>
-          <Divider />
-          <TextField
-            name="organization"
-            variant="standard"
-            label="학교 또는 소속"
-            fullWidth
-            onChange={handleInputChange}
-          />
-
-          {/* ✅ 소재지 */}
-          <Typography variant="body1" mt={2}>
-            소재지
-          </Typography>
-          <AddressSearch
-            selectedLocation={formData.location}
-            setSelectedLocation={(location) =>
-              setFormData((prev) => ({ ...prev, location }))
-            }
-            address={formData.address}
-            setAddress={handleAddressChange}
-          />
-
-          {/* ✅ 직책 선택 */}
-          <Select
-            name="position"
-            variant="standard"
-            displayEmpty
-            fullWidth
-            value={formData.position}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, position: e.target.value }))
-            }
-          >
-            <MenuItem value="" disabled>
-              직책 선택
-            </MenuItem>
-            <MenuItem value="STUDENT">학생</MenuItem>
-            <MenuItem value="BUSINESS">사업자</MenuItem>
-          </Select>
-
-          {/* ✅ 증빙사진 (파일 업로드) */}
-          <Typography variant="body2" mt={2}>
-            증빙사진 (재직 또는 학위증명서)
-          </Typography>
-          <Box width="100%" display="flex" justifyContent="center" mt={1}>
-            <label
-              htmlFor="file-upload"
-              style={{ cursor: "pointer", width: "100%" }}
-            >
-              <Image
-                src="/images/common/select-file.png"
-                height={172}
-                width={408}
-                style={{ minWidth: "100%", height: "auto" }}
-                alt=""
-              />
-            </label>
-            <input
-              id="file-upload"
-              type="file"
-              hidden
-              onChange={handleFileUpload}
+          <Stack gap={1.5}>
+            <TextField
+              name="organization"
+              variant="standard"
+              label="소속정보"
+              fullWidth
+              onChange={handleInputChange}
             />
-          </Box>
+            <AddressSearch
+              label="소재지"
+              selectedLocation={formData.location}
+              setSelectedLocation={(location) =>
+                setFormData((prev) => ({ ...prev, location }))
+              }
+              address={formData.address}
+              setAddress={handleAddressChange}
+            />
+
+            <TextField
+              name="name"
+              variant="standard"
+              label="이름"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <FormControl>
+              <InputLabel variant="standard" htmlFor="position-select">
+                직책
+              </InputLabel>
+              <Select
+                name="position"
+                variant="standard"
+                displayEmpty
+                fullWidth
+                value={formData.position}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, position: e.target.value }))
+                }
+                inputProps={{
+                  id: "position-select",
+                }}
+              >
+                <MenuItem value="" disabled>
+                  {/* 직책 선택 */}
+                </MenuItem>
+                <MenuItem value="STUDENT">학생</MenuItem>
+                <MenuItem value="BUSINESS">사업자</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              name="phoneNumber"
+              variant="standard"
+              label="휴대폰 번호"
+              fullWidth
+              onChange={handleInputChange}
+            />
+          </Stack>
+
+          <Stack>
+            <Typography variant="body2" mt={2}>
+              증빙자료(명함 또는 재직증명서)
+            </Typography>
+            <Box width="100%" display="flex" justifyContent="center" mt={1}>
+              <label
+                htmlFor="file-upload"
+                style={{ cursor: "pointer", width: "100%" }}
+              >
+                <Image
+                  src="/images/common/select-file.png"
+                  height={172}
+                  width={408}
+                  style={{ minWidth: "100%", height: "auto" }}
+                  alt=""
+                />
+              </label>
+              <input
+                id="file-upload"
+                type="file"
+                hidden
+                onChange={handleFileUpload}
+              />
+            </Box>
+          </Stack>
           {uploadedFiles.length > 0 && (
             <Box mt={2}>
               <Stack
